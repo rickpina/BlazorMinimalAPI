cec10d7afa7588fbe4bee1b161321ea32f3ba685
rickpina <rickapina@yahoo.com>
2025-01-18 12:59:33 -0600

    Tiny changes

---
+++ Data Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorMinimalAPI.Data_Models
+{
+    public class Person
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? SSN { get; set; }
+        public string? Occupation { get; set; }
+    }
+}
