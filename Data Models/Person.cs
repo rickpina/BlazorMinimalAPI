0cba526a1b7a0f09a3a573b22c9d57552ff5698f
rickpina <rickapina@yahoo.com>
2025-01-19 12:39:48 -0600

    Added a new component to Generate Random Person Data and added some embedded json files for names.

--- Data Models/Person.cs
+++ Data Models/Person.cs
@@ -1,11 +1,23 @@
-﻿namespace BlazorMinimalAPI.Data_Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorMinimalAPI.Data_Models
 {
     public class Person
     {
+        [Required]
+        [Display(Name = "First Name")]
         public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string? LastName { get; set; }
+
+        [Display(Name = "Date of Birth")]
         public DateTime DateOfBirth { get; set; }
+
+        [Required]
         public string? SSN { get; set; }
+
         public string? Occupation { get; set; }
     }
 }
